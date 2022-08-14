9796e53cb45e28e18112afbbf9543c5073b285d8
marcinlisiecki <marcinlisiecki9@gmail.com>
2022-08-14 15:41:03 +0200

    Change settings sidebar

--- src/components/molecules/SettingsSidebar/SettingsSidebar.tsx
+++ src/components/molecules/SettingsSidebar/SettingsSidebar.tsx
@@ -12,21 +12,37 @@ const sections = [
     path: '/settings/general',
     label: 'General',
     isForLoggedIn: true,
+    newSection: false,
   },
   {
-    path: '/settings/appearance',
-    label: 'Appearance',
-    isForLoggedIn: false,
-  },
-  {
-    path: '/settings/profile',
-    label: 'Profile',
+    path: '/settings/security',
+    label: 'Security',
     isForLoggedIn: true,
+    newSection: false,
   },
   {
     path: '/settings/privacy',
     label: 'Privacy',
     isForLoggedIn: true,
+    newSection: false,
+  },
+  {
+    path: '/settings/appearance',
+    label: 'Appearance',
+    isForLoggedIn: false,
+    newSection: true,
+  },
+  {
+    path: '/settings/typing',
+    label: 'Typing',
+    isForLoggedIn: false,
+    newSection: false,
+  },
+  {
+    path: '/settings/language',
+    label: 'Language',
+    isForLoggedIn: false,
+    newSection: false,
   },
 ] as const;
 
@@ -39,21 +55,24 @@ const SettingsSidebar: FunctionComponent<Props> = () => {
   return (
     <nav className={'w-[200px]'}>
       <ul className={'flex flex-col gap-y-2'}>
-        {sections.map(({ label, path, isForLoggedIn }) => {
+        {sections.map(({ label, path, isForLoggedIn, newSection }) => {
           if (isForLoggedIn && !session?.user) return;
 
           return (
-            <li key={path}>
-              <Link href={path}>
-                <a
-                  className={`text-text-tertiary font-medium hover:text-text-secondary transition inline-block ${
-                    isActive(path) && '!text-text-primary'
-                  }`}
-                >
-                  {label}
-                </a>
-              </Link>
-            </li>
+            <>
+              {newSection && <div className={'w-12 bg-gray-800 h-px my-1'} />}
+              <li key={path}>
+                <Link href={path}>
+                  <a
+                    className={`text-text-tertiary font-medium hover:text-text-secondary transition inline-block ${
+                      isActive(path) && '!text-text-primary'
+                    }`}
+                  >
+                    {label}
+                  </a>
+                </Link>
+              </li>
+            </>
           );
         })}
       </ul>
