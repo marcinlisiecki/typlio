97d490bf9ecbab4a7805c49535ecaf531b4d4fcf
marcinlisiecki <marcinlisiecki9@gmail.com>
2022-08-14 15:42:51 +0200

    Add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,16 @@
+import React, { FunctionComponent } from 'react';
+import PageTemplate from 'components/templates/PageTemplate';
+import PageError from 'components/organisms/PageError';
+
+interface OwnProps {}
+type Props = OwnProps;
+
+const NotFoundPage: FunctionComponent<Props> = () => {
+  return (
+    <PageTemplate title={'404'}>
+      <PageError status={404} />
+    </PageTemplate>
+  );
+};
+
+export default NotFoundPage;
